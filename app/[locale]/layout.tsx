2567549eadcfa7fb747130f0760495dc7ed730e2
dev@erxes.io <dev@erxes.io>
2024-11-14 16:18:53 +0800

    yvaasai

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -66,7 +66,7 @@ export default async function RootLayout({
   const { colors } = uiOptions || {};
 
   return (
-    <html lang={locale || "mn"} suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <head>
         <link
           rel="stylesheet"
