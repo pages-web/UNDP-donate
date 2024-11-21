84334bf71f3895541c9d8485bfcf7a7b4b3a7f90
dev@erxes.io <dev@erxes.io>
2024-11-21 12:31:02 +0800

    a

--- containers/donate/info.tsx
+++ containers/donate/info.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import { CardContent } from "../../app/[locale]/components/ui/card";
 import ErxesForm from "../../app/[locale]/components/modals/WebModal";
-import { Button } from "@/app/[locale]/components/ui/button";
-import { LoadingIcon } from "@/app/[locale]/components/ui/loading";
+import React from "react";
+import { Button } from "../../app/[locale]/components/ui/button";
+import { LoadingIcon } from "../../app/[locale]/components/ui/loading";
 import { ArrowLeftIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { useDonate } from "./donate";
@@ -11,9 +11,9 @@ import { deliveryInfoAtom, donateViewAtom } from "../../store/donate.store";
 import { useAtom, useSetAtom } from "jotai";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { emailZod } from "@/lib/zod";
+import { emailZod } from "../../lib/zod";
 import { z } from "zod";
-import { Form } from "@/app/[locale]/components/ui/form";
+import { Form } from "../../app/[locale]/components/ui/form";
 
 const formSchema = z.object({
   name: z.string().min(2, { message: "Нэрээ бүтнээр нь оруулана уу" }),
@@ -27,8 +27,6 @@ const DonateInfo = () => {
   const t = useTranslations();
   const { description, ...defaultValues } = deliveryInfo;
 
-  const [isFormCompleted, setIsFormCompleted] = useState(false);
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues,
@@ -36,6 +34,7 @@ const DonateInfo = () => {
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     const description = `${values.name} ${values.email}`;
+
     setDeliveryInfo({ ...values, description });
 
     action({
@@ -50,19 +49,6 @@ const DonateInfo = () => {
     });
   };
 
-  const handleFormCompletion = (isCompleted: boolean) => {
-    setIsFormCompleted(isCompleted);
-  };
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (loading || form.formState.isSubmitting) {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-    }
-  }, [loading, form.formState.isSubmitting]);
-
   return (
     <CardContent className="md:pt-0">
       <Form {...form}>
@@ -70,19 +56,12 @@ const DonateInfo = () => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-6 text-black"
         >
-          <ErxesForm
-            className="w-full"
-            formId="eUpBMW"
-            brandId="94ZGAG"
-            onCompleted={() => handleFormCompletion(true)}
-          />
+          <ErxesForm className="w-full" formId="eUpBMW" brandId="94ZGAG" />
           <Button
             type="submit"
             size="lg"
             className="w-full text-white mt-4"
-            disabled={
-              loading || form.formState.isSubmitting || !isFormCompleted
-            }
+            disabled={loading}
           >
             {loading && <LoadingIcon />}
             {t("Үргэлжлүүлэх")}
