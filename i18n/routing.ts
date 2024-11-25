ce929706e4ef639d438ec1e21ad2aa53f200cf58
dev@erxes.io <dev@erxes.io>
2024-11-25 18:08:23 +0800

    a

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,7 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["mn", "en"],
 
-  defaultLocale: "mn",
+  defaultLocale: "en",
 });
 
 export const { Link, redirect, usePathname, useRouter } =
