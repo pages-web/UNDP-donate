d688fa392aae6f136e477818c4e81d64b5328087
dev@erxes.io <dev@erxes.io>
2024-12-09 20:56:29 +0800

    a

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,10 +1,16 @@
 import { defineRouting } from "next-intl/routing";
-import { createNavigation } from "next-intl/navigation";
+import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
+  // A list of all locales that are supported
   locales: ["mn", "en"],
+
+  // Used when no locale matches
   defaultLocale: "mn",
 });
-
+export const locales = ["mn", "en"] as const;
+export const localePrefix = "always";
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } =
-  createNavigation(routing);
+  createSharedPathnamesNavigation({ locales, localePrefix });
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,25 @@
+// import createMiddleware from "next-intl/middleware";
+// import { locales, localePrefix } from "./navigation";
+
+// export default createMiddleware({
+//   defaultLocale: "mn",
+//   localePrefix,
+//   locales,
+//   localeDetection: false,
+// });
+
+// export const config = {
+//   // Match only internationalized pathnames
+//   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)", "/", "/(mn|en)/:path*"],
+// };
 import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
 
-export default createMiddleware({
-  locales: ["en", "mn"],
-
-  defaultLocale: "mn",
+export default createMiddleware(routing, {
+  localeDetection: false,
 });
 
 export const config = {
+  // Match only internationalized pathnames
   matcher: ["/", "/(mn|en)/:path*"],
 };
